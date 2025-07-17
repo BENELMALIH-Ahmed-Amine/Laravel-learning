4b9877fc4655f8a09f2a6a3539ca540e448b5d5d
BENELMALIH-Ahmed-Amine <amine.ahmed.benel@gmail.com>
2025-07-17 10:05:24 +0100

    factory's dada setup

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.index')
+
+@section('content')
+    <h1>Home Content</h1>
+    <div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem nostrum nobis culpa voluptatem tempora illum est recusandae nulla quae reprehenderit nam nisi, dolore, eligendi rerum enim delectus consectetur iusto eum?</p>
+    </div>
+@endsection
